dec73606954ae891e4362e4ffa2148e242149573
dqcer@sina.com <dqcer@sina.com>
2022-10-31 18:45:01 +0800

    添加auth

---
+++ mcdull-support/mcdull-auth/mcdull-auth-provider/src/main/java/com/dqcer/mcdull/auth/provider/AuthApplication.java
@@ -0,0 +1,20 @@
+package com.dqcer.mcdull.auth.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 身份验证应用程序
+ *
+ * @author QinDong
+ * @date 2022/10/31
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class AuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class, args);
+    }
+}
