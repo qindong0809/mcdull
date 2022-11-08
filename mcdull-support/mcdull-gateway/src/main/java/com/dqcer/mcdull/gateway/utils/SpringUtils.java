b3bf08b738b248ec34ad5df3a3990573a27c6d59
dqcer@sina.com <dqcer@sina.com>
2022-11-08 13:45:38 +0800

    添加dict

---
+++ mcdull-support/mcdull-gateway/src/main/java/com/dqcer/mcdull/gateway/utils/SpringUtils.java
@@ -0,0 +1,32 @@
+package com.dqcer.mcdull.gateway.utils;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Deprecated: {@link SpringContextUtil}
+ *
+ * @author QinDong
+ * @date 2022/11/08
+ */
+@Component
+public class SpringUtils implements ApplicationContextAware {
+
+    private static ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    public static <T> T getBean(Class<T> tClass){
+        return applicationContext.getBean(tClass);
+    }
+
+    public static <T> T getBean(String name, Class<T> type) {
+        return applicationContext.getBean(name, type);
+    }
+
+}
