75bfd6d111ff948deda1712a42ef3487e3c3a95f
dqcer <dqcer@sina.com>
2022-11-14 22:31:24 +0800

    添加前端

---
+++ mcdull-bussiness/mcdull-admin/src/main/java/com/dqcer/cloud/mcdul/ladmin/McdullAdminApplication.java
@@ -0,0 +1,13 @@
+package com.dqcer.cloud.mcdul.ladmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McdullAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McdullAdminApplication.class, args);
+    }
+
+}
