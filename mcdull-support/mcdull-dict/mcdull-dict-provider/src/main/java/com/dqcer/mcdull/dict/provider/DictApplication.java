fb84ac76c924e6e1ee79890dc0677f6c2e9772b5
dqcer@sina.com <dqcer@sina.com>
2022-11-14 15:57:38 +0800

    fix: 修复无法启动

--- mcdull-support/mcdull-dict/mcdull-dict-provider/src/main/java/com/dqcer/mcdull/dict/provider/DictApplication.java
+++ mcdull-support/mcdull-dict/mcdull-dict-provider/src/main/java/com/dqcer/mcdull/dict/provider/DictApplication.java
@@ -1,9 +1,13 @@
 package com.dqcer.mcdull.dict.provider;
 
+import com.dqcer.mcdull.framework.web.EnableWebCore;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@MapperScan("com.dqcer.mcdull.dict.provider.web.dao")
+@EnableWebCore
 @EnableDiscoveryClient
 @SpringBootApplication
 public class DictApplication {
