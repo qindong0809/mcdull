ec27b80fcd47a9cf24b974722cdfb38a410c4ad6
dqcer@sina.com <dqcer@sina.com>
2022-11-18 11:47:49 +0800

    features: 格式化日志格式

--- mcdull-support/mcdull-gateway/src/main/java/com/dqcer/mcdull/gateway/filter/LogFilter.java
+++ mcdull-support/mcdull-gateway/src/main/java/com/dqcer/mcdull/gateway/filter/LogFilter.java
@@ -1,22 +1,37 @@
 package com.dqcer.mcdull.gateway.filter;
 
+import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.core.toolkit.IdWorker;
 import com.dqcer.framework.base.constants.HttpHeaderConstants;
 import com.dqcer.mcdull.gateway.utils.IpUtils;
+import com.sun.jndi.toolkit.url.UrlUtil;
+import org.reactivestreams.Publisher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.DataBufferFactory;
+import org.springframework.core.io.buffer.DataBufferUtils;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.http.server.reactive.ServerHttpResponseDecorator;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
 @Component
 public class LogFilter extends AbstractFilter implements GlobalFilter, Ordered {
@@ -38,6 +53,7 @@ public class LogFilter extends AbstractFilter implements GlobalFilter, Ordered {
 
         log.info("请求地址:{} {} 来源Ip: {}", exchange.getRequest().getMethodValue(), exchange.getRequest().getURI(), IpUtils.getRealIp(request));
 
+
         // 构建成一条长 日志，避免并发下日志错乱
         StringBuilder beforeReqLog = new StringBuilder(300);
         // 日志参数
@@ -51,7 +67,6 @@ public class LogFilter extends AbstractFilter implements GlobalFilter, Ordered {
         });
         log.info(beforeReqLog.toString(), beforeReqArgs.toArray());
 
-
         exchange.getAttributes().put(START_TIME, System.currentTimeMillis());
         return chain.filter(exchange).then(Mono.fromRunnable(() -> {
             Long startTime = exchange.getAttribute(START_TIME);
@@ -63,6 +78,25 @@ public class LogFilter extends AbstractFilter implements GlobalFilter, Ordered {
             log.info("耗时: {} ms", executeTime);
             MDC.remove(HttpHeaderConstants.LOG_TRACE_ID);
         }));
+    }
+
+    /**
+     * 从Flux<DataBuffer>中获取字符串的方法
+     *
+     * @return 请求体
+     */
+    private String resolveBodyFromRequest(ServerHttpRequest serverHttpRequest) {
+        //获取请求体
+        Flux<DataBuffer> body = serverHttpRequest.getBody();
+
+        AtomicReference<String> bodyRef = new AtomicReference<>();
+        body.subscribe(buffer -> {
+            CharBuffer charBuffer = StandardCharsets.UTF_8.decode(buffer.asByteBuffer());
+            DataBufferUtils.release(buffer);
+            bodyRef.set(charBuffer.toString());
+        });
+        //获取request body
+        return bodyRef.get();
     }
 
     @Override
