5054eeb051fc846f15686f66649e70bbc7d2b0cb
dqcer@sina.com <dqcer@sina.com>
2023-06-14 10:32:00 +0800

    feat: 添加文件名生成方法

--- mcdull-bussiness/mcdull-business-common/src/main/java/io/gitee/dqcer/mcdull/business/common/FileNameGeneratorUtil.java
+++ mcdull-bussiness/mcdull-business-common/src/main/java/io/gitee/dqcer/mcdull/business/common/FileNameGeneratorUtil.java
@@ -1,12 +1,22 @@
 package io.gitee.dqcer.mcdull.business.common;
 
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.convert.Convert;
 import cn.hutool.core.date.DateUtil;
-import com.alibaba.excel.util.StringUtils;
+import cn.hutool.core.util.NumberUtil;
 import io.gitee.dqcer.mcdull.framework.base.storage.UserContextHolder;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
+/**
+ * @author dqcer
+ */
 public final class FileNameGeneratorUtil {
     private static final String EXCEL = ".xlsx";
 
@@ -17,52 +27,80 @@ public final class FileNameGeneratorUtil {
     }
 
     public static void main(String[] args) {
-        List<String> list =new ArrayList<>();
+        List<String> list = new ArrayList<String>();
         list.add("文件夹(1)(1)");
+        list.add("文件夹(1)(2)");
         list.add("文件夹(3)");
         list.add("文件夹(2)");
         list.add("文件夹(1)");
         list.add("文件夹");
-        String fileName = "文件夹";
-        fileName = buildName(list, fileName, 1);
-        System.out.println("保存起来的文件名称为 :" + fileName);
+
+        String name = "文件夹";
+        String s = windowsFileName(list, name);
+        System.out.println(s);
     }
 
-    private static String buildName(List<String> list, String fileName, int initIndex){
-        if (!list.contains(fileName)) {
-            return fileName;
+    public static String windowsFileName(List<String> oldFileNameList, String newTempFileName) {
+        Set<String> set = new HashSet<>(oldFileNameList);
+        if (set.size() != oldFileNameList.size()) {
+            throw new IllegalArgumentException();
         }
-        if (decide(fileName)) {
-            return fileName = buildName(list, fileName + "(" + initIndex + ")", initIndex++);
+
+        if (!oldFileNameList.contains(newTempFileName)) {
+            return newTempFileName;
         }
-        String numCountString = fileName.substring(fileName.lastIndexOf("(") + 1, fileName.lastIndexOf(")"));
-        int numCount = -1;
-        if (StringUtils.isNumeric(numCountString)) {
-            numCount = Integer.parseInt(numCountString);
+        oldFileNameList.add(newTempFileName);
+        List<String> s = new ArrayList<>();
+        Map<String, Integer> nameMap = new HashMap<>(8);
+        for (String param : oldFileNameList){
+            nameMap.put(param, 0);
         }
-        if (numCount >= 1) {
-            String realName = fileName.substring(0, fileName.lastIndexOf("("));
-            return fileName = buildName(list, realName + "(" + ++numCount +")", 2);
+        for (String name : oldFileNameList){
+            int count = nameMap.get(name);
+            String finalName = "";
+            if (nameMap.containsKey(name)){
+                if (count > 0){
+                    finalName = name + "(" + count + ")";
+                    if (nameMap.containsKey(finalName)){
+                        count++;
+                        finalName = name + "(" + count + ")";
+                    }
+                }else {
+                    finalName = name;
+                }
+            }else {
+                finalName = name;
+            }
+            count++;
+            nameMap.put(name, count);
+            s.add(finalName);
         }
-        return fileName = buildName(list, fileName + "(" + initIndex +")", initIndex++);
+        String fileName = s.stream().filter(i -> !set.contains(i)).findFirst().orElse(null);
+        if (fileName == null) {
+            updateFileNameSuffix(s);
+            fileName = s.stream().filter(i -> !set.contains(i)).findFirst().orElse(null);
+            if (fileName != null){
+                return fileName;
+            }
+            return newTempFileName;
+        }
+        return fileName;
     }
 
-    private static boolean decide(String fileName) {
-        final String left = "(";
-        final String right = ")";
-        if (fileName.lastIndexOf(left) < 0) {
-            return true;
+    private static void updateFileNameSuffix(List<String> s) {
+        Map<String, Integer> countMap = s.stream().collect(Collectors.toMap(k -> k, value -> 1, Integer::sum));
+        List<String> existKeyList = countMap.keySet().stream().filter(a -> countMap.get(a) > 1).collect(Collectors.toList());
+        if (CollUtil.isNotEmpty(existKeyList)) {
+            String  existKey = existKeyList.get(0);
+            String numCountString =  existKey.substring( existKey.lastIndexOf("(") + 1,  existKey.lastIndexOf(")"));
+            if (NumberUtil.isNumber(numCountString)) {
+                Integer integer = Convert.toInt(numCountString);
+                String fileNamePrefix =  existKey.substring(0,  existKey.lastIndexOf("("));
+                s.remove(existKey);
+                s.add(fileNamePrefix + "(" + (integer + 1) + ")");
+                updateFileNameSuffix(s);
+            }
         }
-        if (fileName.lastIndexOf(right) < 0) {
-            return true;
-        }
-        if ((fileName.length() - 1) > fileName.lastIndexOf(right)) {
-            return true;
-        }
-        if ((fileName.lastIndexOf(right) - fileName.lastIndexOf(left)) <= 1) {
-            return true;
-        }
-        return false;
     }
 
 }
