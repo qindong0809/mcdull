cbf800f7e2882c9dbc0e7e6effa5f560f27e6214
dqcer@sina.com <dqcer@sina.com>
2024-05-31 18:32:01 +0800

    feat: server monitor

---
+++ mcdull-support/mcdull-uac/mcdull-uac-provider/src/main/java/io/gitee/dqcer/mcdull/uac/provider/config/GitPropertiesComponent.java
@@ -0,0 +1,36 @@
+package io.gitee.dqcer.mcdull.uac.provider.config;
+
+import org.springframework.stereotype.Component;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * git属性组件
+ *
+ * @author dqcer
+ * @since 2024/05/31
+ */
+@Component
+public class GitPropertiesComponent {
+
+    private final Properties properties = new Properties();
+
+    public GitPropertiesComponent() {
+        try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream("git.properties")) {
+            if (inputStream == null) {
+                throw new FileNotFoundException("git.properties not found in the classpath");
+            }
+            properties.load(inputStream);
+        } catch (IOException e) {
+            throw new RuntimeException("Could not load git.properties", e);
+        }
+    }
+
+    public String getCommitId() {
+        return properties.getProperty("git.commit.id");
+    }
+
+}
---
+++ mcdull-support/mcdull-uac/mcdull-uac-provider/src/main/java/io/gitee/dqcer/mcdull/uac/provider/web/controller/ServerMonitorController.java
@@ -0,0 +1,28 @@
+package io.gitee.dqcer.mcdull.uac.provider.web.controller;
+
+
+import cn.dev33.satoken.annotation.SaCheckPermission;
+import io.gitee.dqcer.mcdull.framework.base.wrapper.Result;
+import io.gitee.dqcer.mcdull.uac.provider.web.service.IServerMonitorService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@Tag(name = "服务监控")
+@RequestMapping
+public class ServerMonitorController {
+
+    private IServerMonitorService serviceMonitorService;
+
+    @Operation(summary = "查询服务监控")
+    @GetMapping("/monitor/server")
+    @SaCheckPermission("system:monitor:server")
+    public Result<Map<String, Object>> queryMonitor(){
+        return Result.success(serviceMonitorService.getServers());
+    }
+}
