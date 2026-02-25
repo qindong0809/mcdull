0e5b14697a334c8de9a41e2da6ffd7886e172c76
dqcer <dqcer@sina.com>
2026-02-25 21:05:39 +0800

    fix: update run

--- mcdull-bussiness/mcdull-business-system/system-bootstrap/src/main/java/io/gitee/dqcer/mcdull/system/SystemApplication.java
+++ mcdull-bussiness/mcdull-business-system/system-bootstrap/src/main/java/io/gitee/dqcer/mcdull/system/SystemApplication.java
@@ -5,15 +5,16 @@ import io.gitee.dqcer.mcdull.framework.mysql.EnableDynamicDataSource;
 import io.gitee.dqcer.mcdull.framework.redis.EnableCache;
 import io.gitee.dqcer.mcdull.framework.web.EnableWebCore;
 import io.gitee.dqcer.mcdull.system.provider.config.Ip2RegionListener;
+import org.dromara.x.file.storage.spring.EnableFileStorage;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
-
+@EnableFileStorage
 @EnableWebCore
-@MapperScan(basePackages = {GlobalConstant.MAPPER_PACKAGE})
+@MapperScan(basePackages = {GlobalConstant.MAPPER_PACKAGE, GlobalConstant.MAPPER_PACKAGE_DAO})
 @EnableDynamicDataSource
 @EnableCache
 @SpringBootApplication(scanBasePackages = "io.gitee")
--- mcdull-framework/mcdull-framework-starters/mcdull-framework-starter-web/src/main/java/io/gitee/dqcer/mcdull/framework/web/config/BannerConfig.java
+++ mcdull-framework/mcdull-framework-starters/mcdull-framework-starter-web/src/main/java/io/gitee/dqcer/mcdull/framework/web/config/BannerConfig.java
@@ -2,10 +2,13 @@ package io.gitee.dqcer.mcdull.framework.web.config;
 
 import cn.hutool.core.thread.ThreadUtil;
 import io.gitee.dqcer.mcdull.framework.base.help.LogHelp;
+import jakarta.annotation.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.env.Environment;
 
 import java.util.concurrent.TimeUnit;
 
@@ -19,19 +22,22 @@ public class BannerConfig implements ApplicationRunner {
 
     protected Logger log = LoggerFactory.getLogger(getClass());
 
+    @Resource
+    private Environment environment;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
         ThreadUtil.execute( () -> {
             ThreadUtil.sleep(1, TimeUnit.SECONDS); // 延迟 1 秒
+            String port = environment.getProperty("server.port", "8080");
             LogHelp.info(log,  "\n----------------------------------------------------------\n\t" +
-                            "Druid monitor paged: \t{} \n\t" +
-                            "H2 console: \t{} \n\t" +
-                            "Swagger: \t{} \n\t" +
-                            "Actuator: \t{} \n\t" +
-                            "Monitoring: \t{} \n\t" +
+                            "Druid monitor url: \t{} \n\t" +
+                            "API Doc url: \t{} \n\t" +
+                            "Monitoring url: \t{} \n\t" +
                             "----------------------------------------------------------",
-                    "http://localhost:8090/druid","http://localhost:8090/h2", "http://localhost:8090/doc.html",
-                "http://localhost:8090/actuator", "http://localhost:8090/monitoring"
+                    "http://localhost:" + port + "/druid",
+                "http://localhost:" + port + "/doc.html",
+                "http://localhost:" + port + "/monitoring"
             );
         });
     }
